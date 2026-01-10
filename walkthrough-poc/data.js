ac6623bf5a5f11448afbf99b82e2d67ed331676a
rajushank84 <rajushank84@yahoo.com>
2026-01-11 00:13:01 +0530

    3d walkthrough poc - lahe

---
+++ walkthrough-poc/data.js
@@ -0,0 +1,705 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-outside-steps-facing-inward",
+      "name": "Outside steps facing inward",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": 0.012360540592114333,
+        "pitch": 0.03300636177230132,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.10153784032380742,
+          "pitch": 0.3100963162145476,
+          "rotation": 0,
+          "target": "1-patio-facing-inward"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-patio-facing-inward",
+      "name": "Patio facing inward",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": -0.20344897557548514,
+        "pitch": -0.002347333779335159,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.784861987099906,
+          "pitch": 0.491153457147675,
+          "rotation": 5.497787143782138,
+          "target": "13-outside-steps-facing-outward"
+        },
+        {
+          "yaw": 0.01646710618366498,
+          "pitch": 0.6205609880022642,
+          "rotation": 0,
+          "target": "2-just-inside-facing-inward"
+        },
+        {
+          "yaw": 0.0568852754585798,
+          "pitch": 0.24750317850332948,
+          "rotation": 7.0685834705770345,
+          "target": "3-room-corner"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-just-inside-facing-inward",
+      "name": "Just inside facing inward",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": -0.36651914291881127,
+        "pitch": 0.04323440701564607,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.4483105771188569,
+          "pitch": 0.29844861499912945,
+          "rotation": 0.7853981633974483,
+          "target": "3-room-corner"
+        },
+        {
+          "yaw": -0.3870575888330823,
+          "pitch": 0.5848615100653323,
+          "rotation": 5.497787143782138,
+          "target": "4-bedside-facing-bed"
+        },
+        {
+          "yaw": -2.972274899672305,
+          "pitch": 0.8482788366886673,
+          "rotation": 0,
+          "target": "12-patio-facing-outward"
+        },
+        {
+          "yaw": -2.9968705038738417,
+          "pitch": 0.3868136169712919,
+          "rotation": 5.497787143782138,
+          "target": "13-outside-steps-facing-outward"
+        },
+        {
+          "yaw": -1.1104876783409683,
+          "pitch": 0.38189967094796273,
+          "rotation": 5.497787143782138,
+          "target": "5-near-mirror-facing-closet"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-room-corner",
+      "name": "Room corner",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": 0.11144433753891114,
+        "pitch": 0.007519027307068882,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.6336234103728806,
+          "pitch": 0.32152518461012036,
+          "rotation": 5.497787143782138,
+          "target": "11-doorway-facing-outward"
+        },
+        {
+          "yaw": -0.12464176840689944,
+          "pitch": 0.3873688515316758,
+          "rotation": 0.7853981633974483,
+          "target": "4-bedside-facing-bed"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-bedside-facing-bed",
+      "name": "Bedside facing bed",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": 2.525526177119996,
+        "pitch": 0.08652797138102564,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.8711913061875105,
+          "pitch": 0.33549735674743886,
+          "rotation": 0.7853981633974483,
+          "target": "5-near-mirror-facing-closet"
+        },
+        {
+          "yaw": -1.7446460088544775,
+          "pitch": 0.5399428184987993,
+          "rotation": 0.7853981633974483,
+          "target": "3-room-corner"
+        },
+        {
+          "yaw": 0.3901094070616793,
+          "pitch": 0.49836939692804094,
+          "rotation": 0.7853981633974483,
+          "target": "11-doorway-facing-outward"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-near-mirror-facing-closet",
+      "name": "Near mirror facing closet",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": 1.8255466812609455,
+        "pitch": 0.009465932569442614,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.6421587247958538,
+          "pitch": 0.4547026112082815,
+          "rotation": 13.351768777756625,
+          "target": "6-closet-facing-bathroom"
+        },
+        {
+          "yaw": -1.349984453670249,
+          "pitch": 0.7087937023599302,
+          "rotation": 0,
+          "target": "10-bedside-facing-outward"
+        },
+        {
+          "yaw": -1.8922553464942045,
+          "pitch": 0.3612150422231437,
+          "rotation": 5.497787143782138,
+          "target": "3-room-corner"
+        },
+        {
+          "yaw": -0.775110267381887,
+          "pitch": 0.4208949764826375,
+          "rotation": 0.7853981633974483,
+          "target": "11-doorway-facing-outward"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-closet-facing-bathroom",
+      "name": "Closet facing bathroom",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": -0.6148929210872396,
+        "pitch": -0.028196352401508307,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.7068461101329682,
+          "pitch": 0.4047348593339546,
+          "rotation": 0,
+          "target": "7-bathroom-inside"
+        },
+        {
+          "yaw": 0.6960805516164896,
+          "pitch": 0.531469264293678,
+          "rotation": 0,
+          "target": "9-mirror-side-facing-outward"
+        },
+        {
+          "yaw": 0.45019333929974437,
+          "pitch": 0.29393738028066707,
+          "rotation": 0,
+          "target": "10-bedside-facing-outward"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-bathroom-inside",
+      "name": "Bathroom inside",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": 3.0491611228004203,
+        "pitch": 0.08330565531395173,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.20316823832911624,
+          "pitch": 0.43512034587228143,
+          "rotation": 0,
+          "target": "8-closet-facing-outward"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-closet-facing-outward",
+      "name": "Closet facing outward",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": 1.005726339027639,
+        "pitch": -0.062031975283318275,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.7056061621304117,
+          "pitch": 0.4151381994447281,
+          "rotation": 0,
+          "target": "7-bathroom-inside"
+        },
+        {
+          "yaw": 0.7191138766798648,
+          "pitch": 0.5498486323759195,
+          "rotation": 0,
+          "target": "9-mirror-side-facing-outward"
+        },
+        {
+          "yaw": 0.4972019599972448,
+          "pitch": 0.26232131165570927,
+          "rotation": 0,
+          "target": "10-bedside-facing-outward"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-mirror-side-facing-outward",
+      "name": "Mirror side facing outward",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": -1.4282962572136881,
+        "pitch": 0.08834857085805936,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.6655651707485521,
+          "pitch": 0.49751401837660225,
+          "rotation": 0.7853981633974483,
+          "target": "6-closet-facing-bathroom"
+        },
+        {
+          "yaw": -1.2641423796719842,
+          "pitch": 0.6467290663809777,
+          "rotation": 0,
+          "target": "10-bedside-facing-outward"
+        },
+        {
+          "yaw": -1.8510898593353744,
+          "pitch": 0.3531274308201571,
+          "rotation": 5.497787143782138,
+          "target": "3-room-corner"
+        },
+        {
+          "yaw": -0.7713867980299742,
+          "pitch": 0.4225503696663999,
+          "rotation": 0.7853981633974483,
+          "target": "11-doorway-facing-outward"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-bedside-facing-outward",
+      "name": "Bedside facing outward",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": -0.7397510906529821,
+        "pitch": -0.007519027307068882,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.6260131864255207,
+          "pitch": 0.5060723312675748,
+          "rotation": 5.497787143782138,
+          "target": "3-room-corner"
+        },
+        {
+          "yaw": 0.42828262025261665,
+          "pitch": 0.47946850898044957,
+          "rotation": 0.7853981633974483,
+          "target": "11-doorway-facing-outward"
+        },
+        {
+          "yaw": 1.811822310215506,
+          "pitch": 0.4189184506847159,
+          "rotation": 0.7853981633974483,
+          "target": "5-near-mirror-facing-closet"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "11-doorway-facing-outward",
+      "name": "Doorway facing outward",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": -2.7894947572315676,
+        "pitch": 0.07707002989745604,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.4683329468121258,
+          "pitch": 0.34210365115365526,
+          "rotation": 0.7853981633974483,
+          "target": "3-room-corner"
+        },
+        {
+          "yaw": -0.5589156789351684,
+          "pitch": 0.6339765689942247,
+          "rotation": 5.497787143782138,
+          "target": "4-bedside-facing-bed"
+        },
+        {
+          "yaw": -1.0239128481791653,
+          "pitch": 0.41772741282348846,
+          "rotation": 5.497787143782138,
+          "target": "5-near-mirror-facing-closet"
+        },
+        {
+          "yaw": -2.8836993523798196,
+          "pitch": 0.6709660718727708,
+          "rotation": 0,
+          "target": "12-patio-facing-outward"
+        },
+        {
+          "yaw": -3.0109163206698746,
+          "pitch": 0.359898760024306,
+          "rotation": 5.497787143782138,
+          "target": "13-outside-steps-facing-outward"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "12-patio-facing-outward",
+      "name": "Patio facing outward",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": -2.995328395367091,
+        "pitch": 0.04657324618218972,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.826436879183156,
+          "pitch": 0.49717371705827773,
+          "rotation": 5.497787143782138,
+          "target": "13-outside-steps-facing-outward"
+        },
+        {
+          "yaw": 0.038736036215675895,
+          "pitch": 0.60867895417433,
+          "rotation": 0,
+          "target": "2-just-inside-facing-inward"
+        },
+        {
+          "yaw": 0.06073867206594308,
+          "pitch": 0.23940768743674035,
+          "rotation": 0.7853981633974483,
+          "target": "3-room-corner"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "13-outside-steps-facing-outward",
+      "name": "Outside steps facing outward",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": 2.6067556751205956,
+        "pitch": -0.008018403214240521,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.10425132617853805,
+          "pitch": 0.2735702279368937,
+          "rotation": 0,
+          "target": "1-patio-facing-inward"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
