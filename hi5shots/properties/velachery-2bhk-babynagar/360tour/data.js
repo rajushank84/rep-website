117013715d49fe9771d9e37dad95240a9c6ea4b5
rajushank84 <rajushank84@yahoo.com>
2026-01-11 21:53:59 +0530

    Adding 360 tour files for Velachery 2bhk

---
+++ hi5shots/properties/velachery-2bhk-babynagar/360tour/data.js
@@ -0,0 +1,775 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-2nd-bedroom-window-facing",
+      "name": "2nd bedroom window facing",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": -0.08124463236635471,
+        "pitch": 0.05171516812809074,
+        "fov": 1.4086226747271073
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.9411240164821653,
+          "pitch": 0.4321903431591547,
+          "rotation": 0,
+          "target": "1-2nd-bedroom-attached-bath"
+        },
+        {
+          "yaw": 2.3388660545699675,
+          "pitch": 0.08948704026113496,
+          "rotation": 0.7853981633974483,
+          "target": "7-1st-bedroom-window-facing"
+        },
+        {
+          "yaw": 2.2370310033198093,
+          "pitch": 0.2094714363079575,
+          "rotation": 5.497787143782138,
+          "target": "5-dining-window-facing"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-2nd-bedroom-attached-bath",
+      "name": "2nd bedroom attached bath",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": 0.7490235472859048,
+        "pitch": -0.0024641574541970357,
+        "fov": 1.4086226747271073
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.310692197575447,
+          "pitch": 0.47427327882116366,
+          "rotation": 7.0685834705770345,
+          "target": "2-2nd-bedrom-door-facing"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-2nd-bedrom-door-facing",
+      "name": "2nd bedrom door facing",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": 2.722478193096971,
+        "pitch": -0.0730567204814907,
+        "fov": 1.4086226747271073
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.223163142820919,
+          "pitch": 0.1671651651942625,
+          "rotation": 5.497787143782138,
+          "target": "5-dining-window-facing"
+        },
+        {
+          "yaw": 2.3694718943355637,
+          "pitch": 0.10551959223678864,
+          "rotation": 0.7853981633974483,
+          "target": "7-1st-bedroom-window-facing"
+        },
+        {
+          "yaw": 2.107044373911947,
+          "pitch": 0.495305921142041,
+          "rotation": 4.71238898038469,
+          "target": "1-2nd-bedroom-attached-bath"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-kitchen-counter-facing",
+      "name": "Kitchen counter facing",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": -3.01448398001145,
+        "pitch": 0.051141129187831424,
+        "fov": 1.4086226747271073
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.22754146252056628,
+          "pitch": 0.3000751731088265,
+          "rotation": 0,
+          "target": "7-1st-bedroom-window-facing"
+        },
+        {
+          "yaw": -0.07019753107651816,
+          "pitch": 0.3539409877320647,
+          "rotation": 5.497787143782138,
+          "target": "5-dining-window-facing"
+        },
+        {
+          "yaw": 0.471618209312787,
+          "pitch": 0.3548979416526592,
+          "rotation": 0.7853981633974483,
+          "target": "10-hall-outward-facing"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-kitchen-outward-facing",
+      "name": "Kitchen outward facing",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": 0.04131311093914114,
+        "pitch": 0.06402830339668597,
+        "fov": 1.4086226747271073
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.473046359351148,
+          "pitch": 0.3903511982434633,
+          "rotation": 7.0685834705770345,
+          "target": "10-hall-outward-facing"
+        },
+        {
+          "yaw": 0.219267299635014,
+          "pitch": 0.2944343310841262,
+          "rotation": 0,
+          "target": "7-1st-bedroom-window-facing"
+        },
+        {
+          "yaw": -0.06962061466011171,
+          "pitch": 0.3870548118447097,
+          "rotation": 5.497787143782138,
+          "target": "5-dining-window-facing"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-dining-window-facing",
+      "name": "Dining window facing",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": -3.0714765921807103,
+        "pitch": -0.03940203285949906,
+        "fov": 1.4086226747271073
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.5240096245713701,
+          "pitch": 0.44890312095742857,
+          "rotation": 4.71238898038469,
+          "target": "7-1st-bedroom-window-facing"
+        },
+        {
+          "yaw": 0.8839121848909226,
+          "pitch": 0.44052586374445646,
+          "rotation": 1.5707963267948966,
+          "target": "3-kitchen-counter-facing"
+        },
+        {
+          "yaw": 0.3505427125671332,
+          "pitch": 0.2940698444530305,
+          "rotation": 0.7853981633974483,
+          "target": "0-2nd-bedroom-window-facing"
+        },
+        {
+          "yaw": -0.008993588000414832,
+          "pitch": 0.3150440896478237,
+          "rotation": 0,
+          "target": "10-hall-outward-facing"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-dining-hall-facing",
+      "name": "Dining hall facing",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": 0.034128222080157755,
+        "pitch": 0.0541784539136394,
+        "fov": 1.4086226747271073
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.4429502495529061,
+          "pitch": 0.4693062317164962,
+          "rotation": 4.71238898038469,
+          "target": "7-1st-bedroom-window-facing"
+        },
+        {
+          "yaw": 0.8231252992970024,
+          "pitch": 0.4622582783329978,
+          "rotation": 1.5707963267948966,
+          "target": "3-kitchen-counter-facing"
+        },
+        {
+          "yaw": 0.3460002341010586,
+          "pitch": 0.3403446369771199,
+          "rotation": 0.7853981633974483,
+          "target": "0-2nd-bedroom-window-facing"
+        },
+        {
+          "yaw": -0.020834435645037175,
+          "pitch": 0.3149009662504749,
+          "rotation": 0,
+          "target": "10-hall-outward-facing"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-1st-bedroom-window-facing",
+      "name": "1st bedroom window facing",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": -3.06789407550332,
+        "pitch": -0.01723838937603439,
+        "fov": 1.4086226747271073
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.4692357773158813,
+          "pitch": 0.5010114303325075,
+          "rotation": 0.7853981633974483,
+          "target": "6-dining-hall-facing"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-1st-bedroom-outward-facing",
+      "name": "1st bedroom outward facing",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": 1.420811771863658,
+        "pitch": 0.1132808444710598,
+        "fov": 1.4086226747271073
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.3784925051096906,
+          "pitch": 0.5181356888555655,
+          "rotation": 0.7853981633974483,
+          "target": "6-dining-hall-facing"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-hall-inward-facing",
+      "name": "Hall inward facing",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.9301202494530303,
+          "pitch": 0.28733964570660575,
+          "rotation": 5.497787143782138,
+          "target": "13-doorway-outward-facing"
+        },
+        {
+          "yaw": 0.8480403814626207,
+          "pitch": 0.39710175272205994,
+          "rotation": 0,
+          "target": "11-1st-bathroom"
+        },
+        {
+          "yaw": -0.24308152678875494,
+          "pitch": 0.24820155170958103,
+          "rotation": 0,
+          "target": "5-dining-window-facing"
+        },
+        {
+          "yaw": -0.5679921828734145,
+          "pitch": 0.27858433249089387,
+          "rotation": 4.71238898038469,
+          "target": "3-kitchen-counter-facing"
+        },
+        {
+          "yaw": 0.015553711589074481,
+          "pitch": 0.3315210110836109,
+          "rotation": 1.5707963267948966,
+          "target": "7-1st-bedroom-window-facing"
+        },
+        {
+          "yaw": -0.6914560672711811,
+          "pitch": 0.4301784042428949,
+          "rotation": 3.9269908169872414,
+          "target": "0-2nd-bedroom-window-facing"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-hall-outward-facing",
+      "name": "Hall outward facing",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": 1.6722828819280124,
+        "pitch": 0.004925254107437382,
+        "fov": 1.4086226747271073
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.8541963781651756,
+          "pitch": 0.3996740458377097,
+          "rotation": 0,
+          "target": "11-1st-bathroom"
+        },
+        {
+          "yaw": 1.9370009220257085,
+          "pitch": 0.3005395156104207,
+          "rotation": 5.497787143782138,
+          "target": "13-doorway-outward-facing"
+        },
+        {
+          "yaw": -0.2480164401907956,
+          "pitch": 0.26877146214914127,
+          "rotation": 0,
+          "target": "5-dining-window-facing"
+        },
+        {
+          "yaw": -0.5848720157782825,
+          "pitch": 0.26559641712148974,
+          "rotation": 4.71238898038469,
+          "target": "3-kitchen-counter-facing"
+        },
+        {
+          "yaw": 0.04808376501162215,
+          "pitch": 0.3362846713145604,
+          "rotation": 1.5707963267948966,
+          "target": "7-1st-bedroom-window-facing"
+        },
+        {
+          "yaw": -0.7148051801753184,
+          "pitch": 0.4266027819992644,
+          "rotation": 3.9269908169872414,
+          "target": "0-2nd-bedroom-window-facing"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "11-1st-bathroom",
+      "name": "1st bathroom",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": -0.04247297037693798,
+        "pitch": 0.054079496288760964,
+        "fov": 1.4086226747271073
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.7622059679612274,
+          "pitch": 0.5285929842547166,
+          "rotation": 5.497787143782138,
+          "target": "9-hall-inward-facing"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "12-doorway-inward-facing",
+      "name": "Doorway inward facing",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": -3.139731929340437,
+        "pitch": -0.06654930039027995,
+        "fov": 1.4086226747271073
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.0899930141683143,
+          "pitch": 0.20370499310500634,
+          "rotation": 0.7853981633974483,
+          "target": "12-doorway-inward-facing"
+        },
+        {
+          "yaw": -1.485212027548414,
+          "pitch": 0.4226087660062845,
+          "rotation": 0,
+          "target": "15-staircase-outward-facing"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "13-doorway-outward-facing",
+      "name": "Doorway outward facing",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": -1.5340045283980697,
+        "pitch": 0.13498627961493526,
+        "fov": 1.4086226747271073
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.491325773738339,
+          "pitch": 0.39507404699847726,
+          "rotation": 0,
+          "target": "15-staircase-outward-facing"
+        },
+        {
+          "yaw": 3.0462608258147714,
+          "pitch": 0.21776286267957623,
+          "rotation": 0.7853981633974483,
+          "target": "9-hall-inward-facing"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "14-staircase-inward-facing",
+      "name": "Staircase inward facing",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": 2.5883600545315035,
+        "pitch": 0.017231091185822223,
+        "fov": 1.4086226747271073
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.990448126717707,
+          "pitch": 0.2501387413565226,
+          "rotation": 0.7853981633974483,
+          "target": "12-doorway-inward-facing"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "15-staircase-outward-facing",
+      "name": "Staircase outward facing",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.9694166823521364,
+          "pitch": 0.25977663625134717,
+          "rotation": 0.7853981633974483,
+          "target": "12-doorway-inward-facing"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Velachery 2BHK",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
